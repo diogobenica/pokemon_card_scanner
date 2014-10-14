8874c88af56aedd76491c40cd001a531f1fb849a
Diogo Benicá <diogo.pereira@abril.com.br>
2014-10-14 16:32:31 -0300

    [WIP] Trying to get corners.

--- teste.rb
+++ teste.rb
@@ -6,15 +6,34 @@ require "RMagick"
 require "byebug"
 # require "imgkit"
 
-session = AVCapture::Session.new
-dev     = AVCapture.devices.find(&:video?)
+# session = AVCapture::Session.new
+# dev     = AVCapture.devices.find(&:video?)
 
-p dev.name
-p dev.video?
+# p dev.name
+# p dev.video?
 
-session.run_with(dev) do |connection|
-  sleep 4
-  File.open("scan.jpg", 'wb') { |f|
-    f.write connection.capture
-  }
+# session.run_with(dev) do |connection|
+#   sleep 4
+#   File.open("scan.jpg", 'wb') { |f|
+#     f.write connection.capture
+#   }
+# end
+
+height = 441
+width = 312
+
+def distance a, b
+  Math.sqrt(((a.x - b.x) ** 2) + ((a.y - b.y) ** 2))
 end
+# probably a better way, but care =~ 0
+def clockwise points
+  ul = Struct.new(:x, :y).new 0, 0
+  upper_left = points.min_by { |point| distance point, ul }
+  until points.first == upper_left
+    points = points.rotate
+  end
+  points
+end
+
+include Magick
+include OpenCV
