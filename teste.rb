0342bf39eac3e36a2f526f98657b4f2e4b2cbdaa
Diogo Benicá <diogo.pereira@abril.com.br>
2014-10-14 16:30:56 -0300

    Captures images from webcam (Mac).

--- teste.rb
+++ teste.rb
@@ -5,3 +5,16 @@ require 'opencv'
 require "RMagick"
 require "byebug"
 # require "imgkit"
+
+session = AVCapture::Session.new
+dev     = AVCapture.devices.find(&:video?)
+
+p dev.name
+p dev.video?
+
+session.run_with(dev) do |connection|
+  sleep 4
+  File.open("scan.jpg", 'wb') { |f|
+    f.write connection.capture
+  }
+end
